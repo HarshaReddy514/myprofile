aff697ecf70ebf8bfb944fd675f7f7e0d5bf2e0d
HarshaReddy514 <devilharsha514@gmail.com>
2024-01-23 12:38:54 +0530

    Added resume page

--- About.py
+++ About.py
@@ -4,24 +4,21 @@ from PIL import Image
 
 def main():
 
-    st.set_page_config(page_title="Harsha Vardhan Reddy", page_icon="🧑‍💻", layout="centered", initial_sidebar_state="auto", menu_items=None)
+    st.set_page_config(page_title="Harsha", page_icon="🧑‍💻", layout="wide", initial_sidebar_state="expanded", menu_items=None)
 
     image = "https://media.licdn.com/dms/image/D5603AQFSvTxudNDn1Q/profile-displayphoto-shrink_800_800/0/1633344799157?e=1711584000&v=beta&t=MQ3QWWgYwXQ-2rVFliVK87a-6M1LYKk995hpPwbLxOE"
-    st.image(image, width=300)
 
-    st.title("Harsha Vardhan Reddy")
-    st.subheader("Senior Software Engineer")
+    col1, col2 = st.columns(2)
 
-    st.sidebar.markdown('''
-- [LinkedIn](https://www.linkedin.com/in/harshavardhanreddy-bommareddy/)
-- [Medium](https://harshavardhan-reddy.medium.com)
-''', unsafe_allow_html=True)
+    with col1:
+        st.image(image, width=250)
 
-    st.markdown("""
-    ## Experienced IT Professional with a Passion for Innovation
-
-    Welcome to my corner of the digital realm! With 7 years in the IT industry, I've honed my skills as a tech enthusiast and problem solver. My journey has been fueled by a relentless curiosity and a commitment to staying at the forefront of technological advancements.
-    """)
+    with col2:
+        st.title("Harsha Vardhan Reddy")
+        st.write("Senior Software Engineer")
+        st.markdown("""
+            Welcome to my corner of the digital realm! With 7 years in the IT industry, I've honed my skills as a tech enthusiast and problem solver. My journey has been fueled by a relentless curiosity and a commitment to staying at the forefront of technological advancements.
+        """)
 
     st.markdown("### Technical Expertise:")
     st.markdown("""
@@ -39,13 +36,6 @@ def main():
     st.markdown("### Beyond the Code:")
     st.markdown("""
     My journey is not just about lines of code. It's about collaborating, problem-solving, and adding real value to every project I undertake. Let's not just meet expectations; let's exceed them together.
-    """)
-
-    st.markdown("### Let's Connect:")
-    st.markdown("""
-    Explore the possibilities of what we can achieve together. Whether you're looking to streamline your development process, troubleshoot a challenging issue, or embark on a new project, I'm ready to be your tech partner.
-
-    Feel free to reach out, and let's turn your tech visions into reality.
     """)
 
 if __name__ == "__main__":
