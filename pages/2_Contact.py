06964db2cfa42f8372aa52346de52bf1011e25fd
HarshaReddy514 <devilharsha514@gmail.com>
2024-01-23 12:54:24 +0530

    Added contact page

---
+++ pages/2_Contact.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+
+    st.set_page_config(page_title="Harsha", page_icon="🧑‍💻", layout="wide", initial_sidebar_state="expanded", menu_items=None)
+
+    st.header("Contact Me")
+    st.markdown("[LinkedIn](https://www.linkedin.com/in/harshavardhanreddy-bommareddy/)")
+    st.markdown("[Email](mailto:harshavardhan.samba@gmail.com)")
+    st.write("Mobile : +91-9542941323")
+
+if __name__ == "__main__":
+    main()
